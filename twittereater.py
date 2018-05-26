e4bf8d429e7c57a76eac3f80a904b1d5bb1a9e98
Tim Chan <tim@chan.so>
2018-05-26 21:43:47 +1000

    Clean up twittereater.py

--- twittereater.py
+++ twittereater.py
@@ -2,14 +2,10 @@ import csv
 f = open('tweets.csv', 'r', encoding='utf-8')
 tweets = []
 reader = csv.reader(f,quotechar='"')
-next(reader)
-#for row in reader:
-#    tweets.append((row[5]))
+next(reader) #get rid of the twitter header
 
-thefile = open('tweetdata.csv', 'w')
+
+tweetarchive = open('tweetdata.csv', 'w')
 for row in reader:
-    thefile.write("'%s'," % (row[5]))
-#with open('filename.text', 'w') as myfile:
-#    wr = csv.writer(myfile, delimiter=",",quotechar="'")
-#    for val in tweets:
-#        wr.writerow([val])
+    tweetarchive.write("'%s'," % (row[5]))
+
